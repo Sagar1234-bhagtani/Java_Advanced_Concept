eb70c78952ad5ec20be6f12493baf3c8c79cd0f1
Sagar1234-bhagtani <sagarbhagtani76@gmail.com>
2023-11-15 08:54:19 +0530

    Added Lambda exercise also

--- src/Practice/Generic_Java.java
+++ src/Practice/Generic_Java.java
@@ -3,23 +3,14 @@ package Practice;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Generic_Java {
-	static <T> List<T> print_kra(T array[]) {
-		List<T> mm=new ArrayList<>();
-		for(T p:array) {
-			mm.add(p);
-		}
-		return mm;
+class Generic_Java
+{
+	public static void main(String[] args) {
+     first=0;
+     last=le
+	}
 		
 	}
-	public static void main(String[] args) {
-		Integer a[]= {1,2,3,4,5,6,8};
-		String s[]= {"Mohan", "Sohan","Payal"};
-		List<Integer> lll=print_kra(a);
-		List<String> ll=print_kra(s);
-		lll.forEach(r->System.out.println(r));
-		ll.forEach(r->System.out.println(r));
-		System.out.println("Trying for second Commit");
-	}
+	
+	
 
-}
